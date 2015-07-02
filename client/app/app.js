1275072c623e2d2a5a5353e188085e0444081481
edder <edder.salazar@xpectra.com>
2015-07-02 16:36:19 -0430

    Instalando angular material

--- client/app/app.js
+++ client/app/app.js
@@ -4,13 +4,28 @@ angular.module('citasmdsApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ngMaterial'
 ])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .otherwise({
-        redirectTo: '/'
-      });
+.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .otherwise({
+      redirectTo: '/'
+    });
 
-    $locationProvider.html5Mode(true);
-  });
+  $locationProvider.html5Mode(true);
+})
+.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+}])
+.controller('ListCtrl', function($scope, $mdDialog) {
+    $scope.settings = [
+      {name: 'Pacientes', icon: 'fa fa-users fa-lg'},
+      {name: 'Citas', icon: 'fa fa-calendar fa-lg'},
+      {name: 'Estadísticas', icon: 'fa fa-area-chart fa-lg'},
+      {name: 'Reportes', icon: 'fa fa-database fa-lg'},
+      {name: 'Configuración', icon: 'fa fa-gears fa-lg'},
+    ];
+});
